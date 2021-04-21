f62baba4a5ad7dfa0aaaf366e827c3ef5fde8a62
Nato Boram <nato.boram@gmail.com>
2021-04-21 19:27:17 -0400

    JQueryStatic can't be exported

--- src/jquery.ts
+++ src/jquery.ts
@@ -1,4 +1,5 @@
-export interface JQueryStatic {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface JQueryStatic {
   flavrNotif(
     content: string,
     options?: Record<string, unknown>,
