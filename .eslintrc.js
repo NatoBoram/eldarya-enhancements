b711fe9f0cb6a9b93456f1d36105756eda76e5c5
Nato Boram <nato.boram@gmail.com>
2021-04-23 00:51:42 -0400

    a bit more prettier and eslinter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,23 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+    ecmaVersion: 2020,
+  },
   plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier",
+    "plugin:prettier/recommended",
   ],
   rules: {
     "@typescript-eslint/no-non-null-assertion": "off", // Breaks code imported from Eldarya.
     "@typescript-eslint/no-var-requires": "off", // Breaks config files with `= require()`.
     "no-undef": "off", // Breaks config files with `module.exports`.
+    // "@typescript-eslint/member-ordering": "error",
+    "prettier/prettier": "error",
   },
 };
