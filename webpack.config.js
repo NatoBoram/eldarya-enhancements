e5cc06e119c4daf65f42228a458adca22b6b528b
Nato Boram <nato.boram@gmail.com>
2021-04-26 18:54:00 -0400

    Bump to 0.0.7

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
 // ==UserScript==
 // @name         Eldarya Enhancements
 // @namespace    https://gitlab.com/NatoBoram/eldarya-enhancements
-// @version      0.0.6
+// @version      0.0.7
 // @author       Nato Boram
 // @description  Enhances the user experience of Eldarya.
 // @icon         https://gitlab.com/NatoBoram/eldarya-enhancements/-/raw/master/images/avatar.png
