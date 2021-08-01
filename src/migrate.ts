874ae4b47faa1ef9dc21538aff184b43fee04093
Nato Boram <nato.boram@gmail.com>
2021-08-01 18:27:26 -0400

    Script installed notification

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,12 +1,20 @@
 import { LocalStorage } from "./local_storage/local_storage"
 
 export function migrate(): void {
-  if (LocalStorage.version && LocalStorage.version !== GM.info.script.version) {
-    if (GM.info.script.version === "1.2.0") {
-      LocalStorage.sales = []
-      $.flavrNotif("Migrated to v1.2.0. Your sales history was erased.")
-    }
+  if (LocalStorage.version === GM.info.script.version) return
+
+  if (GM.info.script.version === "1.2.0") {
+    LocalStorage.sales = []
+    $.flavrNotif("Migrated to v1.2.0. Your sales history was erased.")
+  } else {
+    installed()
   }
 
   LocalStorage.version = GM.info.script.version
+}
+
+function installed(): void {
+  $.flavrNotif(
+    `<strong>${GM.info.script.name}</strong> v<strong>${GM.info.script.version}</strong> installed!`
+  )
 }
