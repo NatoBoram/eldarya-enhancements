22a1747a7f717c7a3e44520984802e1db0f7f037
Nato Boram <nato.boram@gmail.com>
2021-11-09 21:43:18 -0500

    ✨ Add rarity marker and sort wishlist by rarity

--- src/marketplace/interfaces/market_entry.ts
+++ src/marketplace/interfaces/market_entry.ts
@@ -1,9 +1,11 @@
+import type { Rarity } from "../enums/rarity.enum"
 import type { BuyNowPrice } from "./buy_now_price"
 import type { CurrentPrice } from "./current_price"
 import type { MarketEntryDataSet } from "./market_entry_data_set"
 
 export interface MarketEntry extends MarketEntryDataSet {
   icon: string
+  rarity: Rarity
   name: string
   /** Subtitle of a market item under its name */
   abstractType?: string
