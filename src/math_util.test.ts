18ba835192a30421351dfc3f08dfd79fdaf28f21
Nato Boram <nato.boram+git@gmail.com>
2024-07-01 15:23:05 -0400

    👷 Add GitHub Workflows

---
+++ src/math_util.test.ts
@@ -0,0 +1,13 @@
+import { describe, test } from "vitest"
+import { MathUtil } from "./math_util"
+
+describe("MathUtil", () => {
+  test("randomBetween", ({ expect }) => {
+    const result = MathUtil.randomBetween(0, 0)
+    expect(result).toBe(0)
+  })
+
+  test("phi", ({ expect }) => {
+    expect(MathUtil.phi).toBe(1.618033988749895)
+  })
+})
